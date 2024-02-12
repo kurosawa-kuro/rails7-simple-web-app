24a17d515617d5738bbad504b773c96c55fede9b
ユーザー名 <>
2024-02-12 03:47:12 +0000

    a

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+# ユーザーのサンプルデータ
+users = User.create([
+  { name: '田中 太郎', email: 'tanaka@example.com', password: 'password', is_admin: false, avatar: 'tanaka.png' },
+  { name: '鈴木 一郎', email: 'suzuki@example.com', password: 'password', is_admin: false, avatar: 'suzuki.png' },
+  { name: '佐藤 次郎', email: 'sato@example.com', password: 'password', is_admin: true, avatar: 'sato.png' }
+])
+
+# カテゴリーのサンプルデータ
+categories = Category.create([
+  { title: '勉強' },
+  { title: '仕事' },
+  { title: '趣味' }
+])
+
+# Todo項目のサンプルデータ
+todos = Todo.create([
+  { user: users.first, title: 'Rails勉強会の資料作成', categories: [categories.first,categories.second,categories.third] },
+  { user: users.second, title: 'プロジェクトの進捗報告', categories: [categories.second,categories.third] },
+  { user: users.third, title: '新しいギターの購入', categories: [categories.third] }
+])
+
+# 中間テーブルのサンプルデータは、Todo作成時にカテゴリーを指定することで自動的に作成されます。
